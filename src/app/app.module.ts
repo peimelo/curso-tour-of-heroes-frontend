27be350ff27e57061a70a87084774f726bc066f7
Paulo Melo <peimelo@gmail.com>
2022-01-04 23:00:26 -0300

    Adicionando Navegação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
@@ -21,9 +25,13 @@ import { MessagesComponent } from './messages/messages.component';
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
+    FlexLayoutModule,
     MatButtonModule,
     MatExpansionModule,
+    MatIconModule,
     MatToolbarModule,
+    MatTooltipModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent],

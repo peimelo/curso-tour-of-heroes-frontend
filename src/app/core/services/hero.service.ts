3e72d51bf39fa84a89ac74fc2c3ad3be0b3ea631
Paulo Melo <peimelo@gmail.com>
2022-01-19 22:48:19 -0300

    Create Hero

--- src/app/core/services/hero.service.ts
+++ src/app/core/services/hero.service.ts
@@ -27,20 +27,25 @@ export class HeroService {
   getOne(id: number): Observable<Hero> {
     return this.http
       .get<Hero>(`${this.heroesUrl}/${id}`)
-      .pipe(
-        tap((hero) => this.log(`fetched hero id=${id} and name=${hero.name}`))
-      );
+      .pipe(tap((hero) => this.log(`fetched ${this.descAttributes(hero)}`)));
+  }
+
+  // POST /heroes
+  create(hero: Hero): Observable<Hero> {
+    return this.http
+      .post<Hero>(this.heroesUrl, hero)
+      .pipe(tap((hero) => this.log(`created ${this.descAttributes(hero)}`)));
   }
 
   // PUT /heroes/id
   update(hero: Hero): Observable<Hero> {
     return this.http
       .put<Hero>(`${this.heroesUrl}/${hero.id}`, hero)
-      .pipe(
-        tap((hero) =>
-          this.log(`updated hero id=${hero.id} and name=${hero.name}`)
-        )
-      );
+      .pipe(tap((hero) => this.log(`updated ${this.descAttributes(hero)}`)));
+  }
+
+  private descAttributes(hero: Hero): string {
+    return `Hero ID=${hero.id} and Name=${hero.name}`;
   }
 
   private log(message: string): void {
--- src/app/heroes/components/hero-detail/hero-detail.component.ts
+++ src/app/heroes/components/hero-detail/hero-detail.component.ts
@@ -11,6 +11,7 @@ import { HeroService } from '../../../core/services/hero.service';
 })
 export class HeroDetailComponent implements OnInit {
   hero!: Hero;
+  isEditing!: boolean;
 
   constructor(
     private heroService: HeroService,
@@ -23,9 +24,17 @@ export class HeroDetailComponent implements OnInit {
   }
 
   getHero(): void {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
+    const paramId = this.route.snapshot.paramMap.get('id');
 
-    this.heroService.getOne(id).subscribe((hero) => (this.hero = hero));
+    if (paramId === 'new') {
+      this.isEditing = false;
+      this.hero = { name: '' } as Hero;
+    } else {
+      this.isEditing = true;
+
+      const id = Number(paramId);
+      this.heroService.getOne(id).subscribe((hero) => (this.hero = hero));
+    }
   }
 
   goBack(): void {
@@ -36,7 +45,11 @@ export class HeroDetailComponent implements OnInit {
     return !!this.hero.name.trim();
   }
 
-  save(): void {
+  create(): void {
+    this.heroService.create(this.hero).subscribe(() => this.goBack());
+  }
+
+  update(): void {
     this.heroService.update(this.hero).subscribe(() => this.goBack());
   }
 }
